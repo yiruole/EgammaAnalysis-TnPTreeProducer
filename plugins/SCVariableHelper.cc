3d4be6c66e3a68e9b1b5bcebca9ea78f19399d67
ruoleyi <ruole.yi@cern.ch>
2022-08-01 15:15:47 +0200

    uploaded files

--- plugins/SCVariableHelper.cc
+++
@@ -1,8 +0,0 @@
-#include "SCVariableHelper.h"
-
-#include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-
-typedef SCVariableHelper<reco::RecoEcalCandidate> RecoEcalCandidateVariableHelper;
-DEFINE_FWK_MODULE(RecoEcalCandidateVariableHelper);
